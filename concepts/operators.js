4b87c72115da7f3963b342918bbedeea4b706b4b
VictorSoto18 <sotovictorbmx@gmail.com>
2024-09-12 13:45:02 -0600

    02 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,35 @@
+// Operadores Matematicos
+// + y -
+//* y /
+// % y ** Modúlo y Potencia
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+// Operadores de asignación
+let a = 10;
+a += 5; // a = a + 5
+a -= 5; // a = a - 5
+a *= 5; // a = a * 5
+a /= 5; // a = a / 5
+a %= 5; // a = a % 5
+a **= 5; // a = a ** 5
+console.log(a);
+
+// Operadores de Comparación
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+// Operadores Lógicos
+console.log(true && true); // and
+console.log(true || false); // or
+console.log(!true); // not
