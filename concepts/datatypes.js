fef85dca37573a4c2b97b08769b0ed89c35023f3
VictorSoto18 <sotovictorbmx@gmail.com>
2024-09-12 13:31:37 -0600

    01 Tipos de Dato Javascript

---
+++ concepts/datatypes.js
@@ -0,0 +1,23 @@
+// Tipos de Datos en javasscript
+// númericos (enteros, decimales)
+console.log(23);
+console.log(typeof 23);
+console.log(3.1416);
+console.log(typeof 3.1416);
+
+// booleanos (true, false)
+console.log(true);
+console.log(typeof true);
+console.log(false);
+console.log(typeof false);
+
+console.log('HELLO');
+console.log(typeof'HELLO');
+
+console.log(undefined);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof [1, 2, 3]);
+console.log(typeof { name: 'jhon'});
+
+
